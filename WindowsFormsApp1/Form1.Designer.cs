4a758007436939bb80c42867c07f13e5b2255567
Rafiul-Islam <rafi.cse.ru@gmail.com>
2020-03-29 12:39:56 +0600

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resumeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(217, 231);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(86, 40);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(466, 231);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(86, 40);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(285, 310);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(189, 55);
+            this.resetButton.TabIndex = 8;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(323, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 54);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0 : 0 : 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(483, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "00";
+            // 
+            // resumeButton
+            // 
+            this.resumeButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resumeButton.Location = new System.Drawing.Point(341, 53);
+            this.resumeButton.Name = "resumeButton";
+            this.resumeButton.Size = new System.Drawing.Size(86, 40);
+            this.resumeButton.TabIndex = 12;
+            this.resumeButton.Text = "Resume";
+            this.resumeButton.UseVisualStyleBackColor = true;
+            this.resumeButton.Click += new System.EventHandler(this.resumeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resumeButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Stop Watch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button resumeButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        int second = 1;
+        int minute = 0;
+        int hour = 0;
+        int miliSecond = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+            resumeButton.Enabled = false;
+            resetButton.Enabled = false;
+
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            startButton.Enabled = false;
+            stopButton.Enabled = false;
+            resumeButton.Enabled = true;
+            resetButton.Enabled = true;
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            miliSecond++;
+
+            if (miliSecond > 10)
+            {
+                second++;
+                miliSecond = 0;
+            }
+
+            if (second > 59)
+            {
+                minute++;
+                second = 0;
+            }
+
+            if (minute > 59)
+            {
+                hour++;
+                minute = 0;
+            }
+
+            label1.Text = hour.ToString() + " : " + minute.ToString() + " : " + second.ToString();
+            label2.Text = miliSecond.ToString();
+
+
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            second = 00;
+            minute = 00;
+            hour = 00;
+            miliSecond = 00;
+
+            label1.Text = "0 : 0 : 0";
+            label2.Text = "00";
+
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            resumeButton.Enabled = false;
+            resetButton.Enabled = false;
+
+        }
+
+        private void resumeButton_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+            resumeButton.Enabled = false;
+            resetButton.Enabled = false;
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            resumeButton.Enabled = false;
+            resetButton.Enabled = false;
+
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
